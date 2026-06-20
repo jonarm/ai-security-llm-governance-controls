471a89451d6d14312a48076ded8c004b397703c9
Jonar Marzan <jonarmarzan3@gmail.com>
2026-06-20 16:33:23 +1000

    feat: add Terraform for Sentinel workspace foundation

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "ai_governance_lab" {
+  name     = "rg-ai-governance-lab"
+  location = var.location
+}
+
+resource "azurerm_log_analytics_workspace" "ai_governance_sentinel" {
+  name                = "law-ai-governance-sentinel"
+  location            = azurerm_resource_group.ai_governance_lab.location
+  resource_group_name = azurerm_resource_group.ai_governance_lab.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_sentinel_log_analytics_workspace_onboarding" "ai_governance_sentinel_onboarding" {
+  workspace_id = azurerm_log_analytics_workspace.ai_governance_sentinel.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "location" {
+  description = "Azure region for the AI governance lab resources"
+  type        = string
+  default     = "eastus"
+}
